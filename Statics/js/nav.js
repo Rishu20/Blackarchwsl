81d6e198a3429c10aa353d3b285187a567bf5ad8
Shushant Rishav <54536673+Rishu20@users.noreply.github.com>
2023-06-21 07:04:31 +0530

    update

---
+++ Statics/js/nav.js
@@ -0,0 +1,25 @@
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".nav-links");
+const links = document.querySelectorAll(".nav-links li");
+
+hamburger.addEventListener('click', () => {
+    // Animate Links
+    navLinks.classList.toggle("open");
+    links.forEach(link => {
+        link.classList.toggle("fade");
+    });
+
+    // Hamburger Animation
+    hamburger.classList.toggle("toggle");
+});
+
+links.forEach(link => {
+    link.addEventListener('click', () => {
+        // Close the nav menu
+        navLinks.classList.remove("open");
+        links.forEach(link => {
+            link.classList.remove("fade");
+        });
+        hamburger.classList.remove("toggle");
+    });
+});
